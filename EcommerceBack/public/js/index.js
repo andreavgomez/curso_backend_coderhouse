bffbf4698a64142c902d8292688ff5ad500cb795
Andrea Gomez <andrea.vgomez27@gmail.com>
2023-11-02 01:07:47 -0300

    Desafio 4

--- EcommerceBack/public/js/index.js
+++ EcommerceBack/public/js/index.js
@@ -18,3 +18,25 @@ socket.on('client-emit', (data) => {
 socket.on('broadcast-emit', (data) => {
   console.log('event broadcast-emit', data);
 });
+
+// Esta función limpia el formulario
+function clearForm() {
+  document.getElementById('title').value = '';
+  document.getElementById('description').value = '';
+  document.getElementById('price').value = '';
+}
+
+function addProduct() {
+  const title = document.getElementById('title').value;
+  const description = document.getElementById('description').value;
+  const price = parseFloat(document.getElementById('price').value);
+  
+  if (title && description && price) {
+    socket.emit('add-product', { title, description, price });
+    clearForm(); // Limpia el formulario después de enviar
+  }
+}
+
+function deleteProduct(productId) {
+  socket.emit('delete-product', productId);
+}
